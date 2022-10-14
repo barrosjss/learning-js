95135406823e44a1106264fcec6b1042d2642522
Jesus Barros <jesus.reinaldo.barros@gmail.com>
2022-10-13 22:24:59 -0500

    18.20 EJERCICIO 20

--- js/codigo.js
+++ js/codigo.js
@@ -35,109 +35,145 @@ function muestraOculta(id) {
 }
 
 // 18.18 EJERCICIO 18
-function tecladoymouse(elEvento) {
-  var evento = elEvento || window.event;
-  switch (evento.type) {
-    case 'mousemove':
-      document.getElementById('info').style.backgroundColor = '#FFFFFF';
-      var ie = navigator.userAgent.toLowerCase().indexOf('msie') != -1;
-      var coordenadaXrelativa, coordenadaYrelativa, coordenadaXabsoluta, coordenadaYabsoluta;
-      if (ie) {
-        if (document.documentElement && document.documentElement.scrollTop) {
-          coordenadaXabsoluta = evento.clientX + document.documentElement.scrollLeft;
-          coordenadaYabsoluta = evento.clientY + document.documentElement.scrollTop;
-        }
-        else {
-          coordenadaXabsoluta = evento.clientX + document.body.scrollLeft;
-          coordenadaYabsoluta = evento.clientY + document.body.scrollTop;
-        }
-      }
-      else {
-        coordenadaXabsoluta = evento.pageX;
-        coordenadaYabsoluta = evento.pageY;
-      }
-      coordenadaXrelativa = evento.clientX;
-      coordenadaYrelativa = evento.clientY;
-      muestratecladoymouse(['Raton', 'Navegador [' + coordenadaXrelativa + ', ' + coordenadaYrelativa + ']', 'Pagina [' + coordenadaXabsoluta + ', ' + coordenadaYabsoluta + ']']);
-      break;
-    case 'keypress':
-      document.getElementById('info').style.backgroundColor = '#CCE6FF';
-      var caracter = evento.charCode || evento.keyCode;
-      var letra = String.fromCharCode(caracter);
-      var codigo = letra.charCodeAt(0);
-      muestratecladoymouse(['Teclado', 'Caracter [' + letra + ']', 'Codigo [' + codigo + ']']);
-      break;
-    case 'click':
-      document.getElementById('info').style.backgroundColor = '#FFFFCC';
-      break;
-  }
-}
+// function tecladoymouse(elEvento) {
+//   var evento = elEvento || window.event;
+//   switch (evento.type) {
+//     case 'mousemove':
+//       document.getElementById('info').style.backgroundColor = '#FFFFFF';
+//       var ie = navigator.userAgent.toLowerCase().indexOf('msie') != -1;
+//       var coordenadaXrelativa, coordenadaYrelativa, coordenadaXabsoluta, coordenadaYabsoluta;
+//       if (ie) {
+//         if (document.documentElement && document.documentElement.scrollTop) {
+//           coordenadaXabsoluta = evento.clientX + document.documentElement.scrollLeft;
+//           coordenadaYabsoluta = evento.clientY + document.documentElement.scrollTop;
+//         }
+//         else {
+//           coordenadaXabsoluta = evento.clientX + document.body.scrollLeft;
+//           coordenadaYabsoluta = evento.clientY + document.body.scrollTop;
+//         }
+//       }
+//       else {
+//         coordenadaXabsoluta = evento.pageX;
+//         coordenadaYabsoluta = evento.pageY;
+//       }
+//       coordenadaXrelativa = evento.clientX;
+//       coordenadaYrelativa = evento.clientY;
+//       muestratecladoymouse(['Raton', 'Navegador [' + coordenadaXrelativa + ', ' + coordenadaYrelativa + ']', 'Pagina [' + coordenadaXabsoluta + ', ' + coordenadaYabsoluta + ']']);
+//       break;
+//     case 'keypress':
+//       document.getElementById('info').style.backgroundColor = '#CCE6FF';
+//       var caracter = evento.charCode || evento.keyCode;
+//       var letra = String.fromCharCode(caracter);
+//       var codigo = letra.charCodeAt(0);
+//       muestratecladoymouse(['Teclado', 'Caracter [' + letra + ']', 'Codigo [' + codigo + ']']);
+//       break;
+//     case 'click':
+//       document.getElementById('info').style.backgroundColor = '#FFFFCC';
+//       break;
+//   }
+// }
 
-function muestratecladoymouse(mensaje) {
-  document.getElementById("info").innerHTML = '<h1>' + mensaje[0] + '</h1>';
-  for (var i = 1; i < mensaje.length; i++) {
-    document.getElementById("info").innerHTML += '<p>' + mensaje[i] + '</p>';
-  }
-}
+// function muestratecladoymouse(mensaje) {
+//   document.getElementById("info").innerHTML = '<h1>' + mensaje[0] + '</h1>';
+//   for (var i = 1; i < mensaje.length; i++) {
+//     document.getElementById("info").innerHTML += '<p>' + mensaje[i] + '</p>';
+//   }
+// }
 
-document.onmousemove = tecladoymouse;
-document.onkeypress = tecladoymouse;
-document.onclick = tecladoymouse;
+// document.onmousemove = tecladoymouse;
+// document.onkeypress = tecladoymouse;
+// document.onclick = tecladoymouse;
 
 // 18.19 EJERCICIO 19
-function zonaPantalla(elEvento) {
+// function zonaPantalla(elEvento) {
+//   var evento = elEvento || window.event;
+
+//   var coordenadaX = evento.clientX;
+//   var coordenadaY = evento.clientY;
+
+//   var dimensiones = tamanoVentanaNavegador();
+//   var tamanoX = dimensiones[0];
+//   var tamanoY = dimensiones[1];
+
+//   var posicionHorizontal = "";
+//   var posicionVertical = "";
+
+//   if (coordenadaX > tamanoX / 2) {
+//     posicionHorizontal = "derecha";
+//   }
+//   else {
+//     posicionHorizontal = "izquierda";
+//   }
+
+//   if (coordenadaY > tamanoY / 2) {
+//     posicionVertical = "abajo";
+//   }
+//   else {
+//     posicionVertical = "arriba";
+//   }
+
+//   muestraZonaPantalla(['Posicion', posicionHorizontal, posicionVertical]);
+// }
+
+// function muestraZonaPantalla(mensaje) {
+//   document.getElementById("info2").innerHTML = '<h1>' + mensaje[0] + '</h1>';
+//   for (var i = 1; i < mensaje.length; i++) {
+//     document.getElementById("info2").innerHTML += '<p>' + mensaje[i] + '</p>';
+//   }
+// }
+
+// function tamanoVentanaNavegador() {
+//   // Adaptada de http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
+//   var dimensiones = [];
+
+//   if (typeof (window.innerWidth) == 'number') {
+//     // No es IE
+//     dimensiones = [window.innerWidth, window.innerHeight];
+//   } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+//     //IE 6 en modo estandar (no quirks)
+//     dimensiones = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+//   } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+//     //IE en modo quirks
+//     dimensiones = [document.body.clientWidth, document.body.clientHeight];
+//   }
+
+//   return dimensiones;
+// }
+
+// document.onclick = zonaPantalla;
+
+// 18.20 EJERCICIO 20
+function limita(elEvento, maximoCaracteres) {
+  var elemento = document.getElementById("texto");
+
+  // Obtener la tecla pulsada 
   var evento = elEvento || window.event;
+  var codigoCaracter = evento.charCode || evento.keyCode;
+  // Permitir utilizar las teclas con flecha horizontal
+  if(codigoCaracter == 37 || codigoCaracter == 39) {
+    return true;
+  }
 
-  var coordenadaX = evento.clientX;
-  var coordenadaY = evento.clientY;
-
-  var dimensiones = tamanoVentanaNavegador();
-  var tamanoX = dimensiones[0];
-  var tamanoY = dimensiones[1];
-
-  var posicionHorizontal = "";
-  var posicionVertical = "";
-
-  if (coordenadaX > tamanoX / 2) {
-    posicionHorizontal = "derecha";
+  // Permitir borrar con la tecla Backspace y con la tecla Supr.
+  if(codigoCaracter == 8 || codigoCaracter == 46) {
+    return true;
+  }
+  else if(elemento.value.length >= maximoCaracteres ) {
+    return false;
   }
   else {
-    posicionHorizontal = "izquierda";
+    return true;
   }
+}
 
-  if (coordenadaY > tamanoY / 2) {
-    posicionVertical = "abajo";
+function actualizaInfo(maximoCaracteres) {
+  var elemento = document.getElementById("texto");
+  var info = document.getElementById("info");
+
+  if(elemento.value.length >= maximoCaracteres ) {
+    info.innerHTML = "Maximo "+maximoCaracteres+" caracteres";
   }
   else {
-    posicionVertical = "arriba";
-  }
-
-  muestraZonaPantalla(['Posicion', posicionHorizontal, posicionVertical]);
-}
-
-function muestraZonaPantalla(mensaje) {
-  document.getElementById("info2").innerHTML = '<h1>' + mensaje[0] + '</h1>';
-  for (var i = 1; i < mensaje.length; i++) {
-    document.getElementById("info2").innerHTML += '<p>' + mensaje[i] + '</p>';
+    info.innerHTML = "Puedes escribir hasta "+(maximoCaracteres-elemento.value.length)+" caracteres adicionales";
   }
 }
-
-function tamanoVentanaNavegador() {
-  // Adaptada de http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
-  var dimensiones = [];
-
-  if (typeof (window.innerWidth) == 'number') {
-    // No es IE
-    dimensiones = [window.innerWidth, window.innerHeight];
-  } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-    //IE 6 en modo estandar (no quirks)
-    dimensiones = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-  } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-    //IE en modo quirks
-    dimensiones = [document.body.clientWidth, document.body.clientHeight];
-  }
-
-  return dimensiones;
-}
-
-document.onclick = zonaPantalla;
